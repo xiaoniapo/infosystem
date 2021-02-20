6b7714ce3141f8b8484da716dedc280a0dcd925d
chen <2463238851@qq.com>
2021-02-20 14:58:48 +0800

    login界面

---
+++ infosystem/src/views/Login.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="login">
+        <div class="login_box">
+            <h2>
+                Login
+            </h2>
+            <div class="user iconfont">
+                <input type="text" placeholder="请输入用户名">
+            </div>
+            <div class="pwd iconfont">
+                <input type="text" placeholder="请输入密码">
+            </div>
+            <div class="login-btn">登录</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+@import "../assets/font.css";
+.login {
+    width: 100vw;
+    height: 100vh;
+    background-image: url("../assets/bg.jpg");
+    background-size: 100% 100%;
+
+    position: relative;
+
+}
+
+.login_box {
+    width: 500px;
+    height: 360px;
+    background-color: rgba(0, 0, 0, .5);
+
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+.login_box h2 {
+    width: 100%;
+    line-height: 35px;
+    color: #fff;
+    font-weight: bold;
+    text-align: center;
+    font-size: 18px;
+    border-bottom: 1px solid #fff;
+}
+.user,
+.pwd {
+    width: 90%;
+    margin: 0 auto;
+}
+
+.user input,
+.pwd input {
+    width: 80%;
+    height: 36px;
+    border-radius: 5px;
+    outline: none;
+    padding-left: 8px;
+    font-size: 16px;
+    margin-left: 16px;
+}
+
+.login-btn {
+    width: 150px;
+    line-height: 40px;
+    text-align: center;
+    border-radius: 20px;
+    margin: 0px auto;
+    color: #fff;
+    background-image: linear-gradient(90deg, red, blue);
+    cursor: pointer;
+}
+</style>
